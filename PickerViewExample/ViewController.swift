f7741a513dedea9e59999ff868dafc160bcc1a9d
Rashid Latif <rashid.latif93@gmail.com>
2020-07-04 12:28:34 +0400

    Picker view

--- PickerViewExample/ViewController.swift
+++ PickerViewExample/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    let height:CGFloat = 20
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        picker.transform = CGAffineTransform(rotationAngle: (-90 * (.pi / 180)))
+
     }
-
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 30
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return height
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        picker.subviews[1].isHidden = true
+        picker.subviews[2].isHidden = true
+        
+        let customView = UIView(frame: CGRect(x: 0, y: -5, width: 70, height: height))
+        var scaleLabelWidth = 20
+        if row % 2 == 0 {
+            scaleLabelWidth = 40
+        }
+        let scaleLabel = UIView(frame: CGRect(x: 0, y: 8, width: scaleLabelWidth, height: 4))
+        scaleLabel.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        customView.addSubview(scaleLabel)
+        
+        let backView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: height))
+        customView.addSubview(backView)
+        backView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        return customView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(row)
+    }
+    
+    
 }
+
 
